926de3da333bf867edaeecdf512b7f312b6e94a2
quodnon <feanorfeanturi@gmail.com>
2015-08-11 00:11:28 +0300

    first tour

--- Untitled.go
+++ Untitled.go
@@ -3,9 +3,17 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
+	"math"
 )
 
 func main() {
 	fmt.Println("Hello world")
-	fmt.Println("Tht time is", time.Now())
+	fmt.Println("Tht time is", time.Now() , rand.Intn(10))
+	fmt.Println(math.Nextafter(5,9))
+	fmt.Println(add(5,3));
+}
+
+func add(x int , y int ) int {
+	return x+y;
 }
