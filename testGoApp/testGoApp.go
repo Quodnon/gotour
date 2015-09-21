47ee433ee26fd67f5c55ae42c22043c4a68c2c51
Mykola <feanorfeanturi@gmail.com>
2015-09-21 12:01:32 +0000

    todos added

--- testGoApp/testGoApp.go
+++ testGoApp/testGoApp.go
@@ -4,7 +4,10 @@ import (
     "net/http"
     "fmt"
     )
-
+//TODO get data from file  separator x , separator a : end of line ; float .
+//TODO convert string to arrays
+//TODO first step
+//TODO print result of step;
 func main ( ) {
 	fmt.Println("hello your ds World");
 }
